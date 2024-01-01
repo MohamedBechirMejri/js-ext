2ecf777457dec18f09f35b64dfa40b548d72910d
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2024-01-01 21:48:11 +0100

    Add merge logic for index.d.ts file

--- build.ts
+++ build.ts
@@ -1,7 +1,15 @@
 import dts from "bun-plugin-dts";
+import fs from "fs";
 
+// @ts-ignore
 await Bun.build({
   entrypoints: ["./index.ts"],
   outdir: "./.build",
   plugins: [dts()],
 });
+
+// merge ./.build/index.d.ts with ./types/globals.d.ts
+const indexDts = fs.readFileSync("./.build/index.d.ts", "utf8");
+const globalsDts = fs.readFileSync("./types/globals.d.ts", "utf8");
+const mergedDts = indexDts + "\n" + globalsDts;
+fs.writeFileSync("./.build/index.d.ts", mergedDts);
