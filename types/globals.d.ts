468e316c95de88a858adb53eef4b78f4d8831093
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2024-01-01 22:08:19 +0100

    Add jsdoc

--- types/globals.d.ts
+++ types/globals.d.ts
@@ -1,6 +1,19 @@
 declare global {
   interface String {
+    /**
+     * Capitalize first letter of a string
+     * @returns {string}
+     * @example
+     * "hello world".CAPITALIZE(); // Hello world
+     */
     CAPITALIZE(): string;
+
+    /**
+     * Flip a string
+     * @returns {string}
+     * @example
+     * "hello world".FLIP(); // dlrow olleh
+     */
     FLIP(): string;
   }
 }
