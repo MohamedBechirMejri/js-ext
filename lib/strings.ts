cbb3156aa2a3d3973ff21d2bbf72d484a197d473
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2024-01-01 19:16:48 +0100

    Add string manipulation functions

---
+++ lib/strings.ts
@@ -0,0 +1,22 @@
+declare global {
+  interface String {
+    CAPITALIZE(): string;
+    FLIP(): string;
+  }
+}
+
+// Capitalize first letter of a string
+export const capitalize = () => {
+  String.prototype.CAPITALIZE = function () {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+  };
+};
+
+// flip a string
+export const flip = () => {
+  String.prototype.FLIP = function () {
+    return this.split("").reverse().join("");
+  };
+};
+
+export default { capitalize, flip };
